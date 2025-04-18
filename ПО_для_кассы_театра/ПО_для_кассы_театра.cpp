f10630be88a8181bf4e8ae22b4739c14fccfbab6
Alex Litvinovich <litvinocih@gmail.com>
2025-04-19 00:09:29 +0300

    The following features were added: - getLogin() and getPassword() functions; - admin authorisation check.
    Future improvements:
    - make password input in asterics or invisible;
    - continue menu and functionality developemnt.

--- ПО_для_кассы_театра/ПО_для_кассы_театра.cpp
+++ ПО_для_кассы_театра/ПО_для_кассы_театра.cpp
@@ -1,14 +1,61 @@
 ﻿// ПО_для_кассы_театра.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <stdio.h>                      
+#include <string.h>
+#include <string>
+
+std::string getLogin()
+{
+    std::string login;
+    std::cout << "Введите логин: ";
+    std::cin >> login;
+    return login;
+}
+std::string getPassword()
+{
+    std::string password;
+    std::cout << "Введите пароль: ";
+    std::cin >> password;
+    return password;
+}
+
+
 
 int main()
 {
     //std::cout << "Hello World!\n";
+    std::string admin_login, admin_password, input_login, input_password;
 
-    std::cout << "Добры дзень!\n";
+    admin_login = "adminus";
+    admin_password = "easy_password";
+
+    bool isMatched = false;
+
+    while (!isMatched)
+    {
+        input_login = getLogin();
+        input_password = getPassword();
+        if (input_login == admin_login && input_password == admin_password)
+        {
+            std::cout << "Авторизация прошла успешно. Вы администратор." << std::endl;
+            isMatched = true;
+        }
+        else
+        {
+            std::cout << "Неверная комбинация логина и пароля. Повторите попытку." << std::endl;
+            std::cin;
+        }
+    }
+
+
+
 }
+
+
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
